940afdba5590704f09b83867a550c1ed798de711
zubairbinshaukat <zubairbinshaukat4455@gmail.com>
2024-09-19 20:27:09 +0500

    authorization complete

---
+++ src/appWrite/auth.js
@@ -0,0 +1,56 @@
+import config from "../env config/config"
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount ({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name)
+            if (userAccount) {
+                //call another method
+                return this.login({email,password})
+            } else {
+                return userAccount 
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return await  this.account.createEmailPasswordSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+    async currentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error;
+        }
+
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService
+
+export default authService
