2175003bd1e6c3dd4180ecf9a3cc92e68a1f8da5
zubairbinshaukat <zubairbinshaukat4455@gmail.com>
2024-09-22 17:31:28 +0500

    authDone

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appWrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .currentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
+  return !loading ? (
     <>
-      Hello
+      <div className="bg-cyan-200 min-h-screen content-between flex flex-wrap">
+        <div className="w-full block">
+          <Header />
+          <main>{/* <Outlet/> */}</main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ):null;
 }
 
-export default App
+export default App;
